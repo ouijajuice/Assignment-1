a348f117d099e41eb0783957a8443b88c0f3a917
ouijajuice <eearls@ltu.edu>
2024-01-11 03:13:38 -0500

    movement work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,17 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float jumpForce;
-    Vector2 move;
-    Rigidbody2D rb;
+    public float maxSpeed;
+
+    private Vector2 lastVelocity;
+    private bool isGrounded;
+    private Vector2 move;
+    private Rigidbody2D rb;
+
+    public Transform wallCheck;
+    private bool onWall;
+    public float wallSlideSpeed;
+    public LayerMask wallLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +26,56 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.AddForce(move*speed*Time.deltaTime);
-        
+        rb.AddForce(move * speed * Time.deltaTime);
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true && IsOnWall() == false)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        move = new Vector2(Input.GetAxisRaw("Horizontal"),0f);
+
+        if (Mathf.Abs(rb.velocity.x) < maxSpeed)
+        {
+            move = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+        else
+        {
+            move = Vector2.zero;
+        }
+
+        lastVelocity = rb.velocity;
+
+        //Debug.Log(IsOnWall());
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            rb.velocity = new Vector2(lastVelocity.x, 0f);
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private bool IsOnWall()
+    {
+        if (Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
