ec209c9a42118747cc7cee2bad64744ebeabd977
ouijajuice <eearls@ltu.edu>
2024-01-13 12:55:17 -0500

    buncha stuf

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float jumpForce;
     public float maxSpeed;
+    public float wallMaxSpeed;
 
     private Vector2 lastVelocity;
     private bool isGrounded;
@@ -26,20 +27,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //adding force to rb in fixed so its not frame dependant i think idk
         rb.AddForce(move * speed * Time.deltaTime);
+
+        //slowing the velocity of the rb when on a wall
+        //this shit kinda sucks probably should change later
+        if (IsOnWall() == true)
+        {
+            rb.velocity = new Vector2(0,rb.velocity.y * wallMaxSpeed);
+        }
     }
     // Update is called once per frame
     void Update()
     {
+        //if space press and on ground and not on wall then add impulse up on rb
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true && IsOnWall() == false)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-
+        
+        //if x velocity is less than the max speed then accept player input
         if (Mathf.Abs(rb.velocity.x) < maxSpeed)
         {
             move = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+            
         }
+        
+        //if not, ignore player input
+        //probably a better way to do this but it works
         else
         {
             move = Vector2.zero;
@@ -52,15 +67,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //ground detection
         if (collision.gameObject.CompareTag("Ground"))
         {
+            //this velocity line is so the player doesn't lose all momentum upon landing
+            //i could just give everything 0 friction but i feel like this is better long term
             rb.velocity = new Vector2(lastVelocity.x, 0f);
+
             isGrounded = true;
         }
     }
 
     private void OnCollisionExit2D(Collision2D other)
     {
+        //detect is player is leaving ground
+        //i forgot why i did this like this but whatever
         if (other.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
@@ -69,6 +90,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsOnWall()
     {
+        //detect if a wall is within range of the wall detection transform
         if (Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer))
         {
             return true;
